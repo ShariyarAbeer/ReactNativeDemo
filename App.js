ddc20b12f72aaa13da7f4ed240f9eac89b6d14e6
ShariyarAbeer <007abeershariyar@gmail.com>
2020-03-06 16:37:55 +0600

    update

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  const [outputText, setoutputText] = useState(
+    "Open up App.js to start working on your app!"
+  );
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button title="Click me" onPress={() => setoutputText("Text Change")} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
